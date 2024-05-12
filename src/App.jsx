e5bfb9d2d45497626577d22da04bde6a9d00096f
RohanGorla <rohangorla2010@gmail.com>
2024-05-12 05:38:49 +0530

    add draw condition and update button glitch

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [chance, setTurn] = useState(false);
+  const [turn, setTurn] = useState(0);
+  const [draw, setDraw] = useState(false);
+  const [chance, setChance] = useState(false);
   const [game, setGame] = useState(false);
   const [winner, setWinner] = useState();
   const [selectX, setSelectX] = useState([]);
   const [selectO, setSelectO] = useState([]);
 
-  if (game) {
+  if (game || draw) {
   } else {
+    let i = 0;
     let wins = [
       [0, 1, 2],
       [3, 4, 5],
@@ -31,6 +34,11 @@ function App() {
       ) {
         selectX.includes(win[0]) ? setWinner("X") : setWinner("O");
         setGame(!game);
+      } else {
+        i++;
+        if (i === 8 && turn === 9) {
+          setDraw(!draw);
+        }
       }
     });
   }
@@ -46,15 +54,18 @@ function App() {
         : setSelectX((prev) => {
             return [...prev, index];
           });
-      setTurn(!chance);
+      setChance(!chance);
+      setTurn((prev) => prev + 1);
     }
   }
 
   function reset() {
     setGame(false);
+    setDraw(false);
+    setTurn(0);
     setSelectO([]);
     setSelectX([]);
-    setTurn(false);
+    setChance(false);
     setWinner();
   }
 
@@ -62,12 +73,12 @@ function App() {
     <div
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
-      <h1 className={game ? "title" : "title nogame"}>
-        {game ? `${winner} WON!` : ""}
+      <h1 className={game || draw ? "title" : "title nogame"}>
+        {draw ? "It's a Draw!" : game ? `${winner} WON!` : ""}
       </h1>
       <p
         style={
-          game
+          game || draw
             ? { transition: "all 1s", opacity: "0" }
             : { transition: "all 1s" }
         }
@@ -112,11 +123,7 @@ function App() {
       </div>
       <button
         onClick={reset}
-        style={
-          game
-            ? { marginTop: "10px", transition: "all 0.5s" }
-            : { transition: "all 0.5s", opacity: "0" }
-        }
+        style={game || draw ? { marginTop: "15px" } : { display: "none" }}
       >
         Play again!
       </button>
